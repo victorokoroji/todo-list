f1825c858e8365a25a57fbb8fd63b3247740c056
unknown <okorojiebube2@gmail.com>
2022-02-23 17:14:17 -0600

    Fix lighthouse error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import './style.css';
-import todoUI from '../modules/todoUi.js';
+// import todoUI from '../modules/todoUi.js';
 import * as todos from '../modules/todos.js';
-
-document.querySelector('.main').innerHTML = todoUI();
 
 document.querySelector('.enter').addEventListener('click', todos.saveTodos);
 
