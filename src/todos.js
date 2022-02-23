c1e5d3f860331aea8f76cc85ff3ab063ae72b6d2
unknown <okorojiebube2@gmail.com>
2022-02-23 13:05:28 -0600

    Populate todos to UI

--- src/todos.js
+++ src/todos.js
@@ -1,3 +1,5 @@
+import dots from './assets/images/dots.png'
+
 class Todo {
 	constructor(description, completed, index) {
 		this.description = description
@@ -11,7 +13,7 @@ const saveTodos = () => {
 	existingTodos = existingTodos === null ? [] : existingTodos
 
 	let inputTodo = document.querySelector('#description').value
-	const todo = new Todo(inputTodo, false, existingTodos.length)
+	const todo = new Todo(inputTodo, false, existingTodos.length + 1)
 
 	existingTodos.push(todo)
 
@@ -21,13 +23,42 @@ const saveTodos = () => {
 	populateTodos()
 }
 
-
 const populateTodos = () => {
 	let existingTodos = JSON.parse(localStorage.getItem('todos'))
 	if (existingTodos !== null && existingTodos.length > 0) {
-     
+		const todoContainer = document.querySelector('.todo-container')
+		todoContainer.innerHTML = ''
+
+		existingTodos.forEach((todo, index) => {
+			const taskList = document.createElement('div')
+			const task = document.createElement('div')
+
+			taskList.classList.add('tasks')
+			task.classList.add('task')
+
+			const input = document.createElement('input')
+			input.type = 'checkbox'
+			input.classList.add('checkbox')
+			input.id = 'complete'
+
+			const item = document.createElement('span')
+			item.classList.add('item')
+
+			const edit = document.createElement('img')
+			edit.setAttribute('src', dots)
+			edit.id = index
+
+			item.innerHTML = todo.description
+
+			todoContainer.appendChild(taskList)
+			taskList.appendChild(task)
+			taskList.appendChild(edit)
+			task.appendChild(input)
+			task.appendChild(item)
+		})
+	} else {
+		document.querySelector('.todo-container').innerHTML = ''
 	}
 }
 
-export { saveTodos }
-
+export { saveTodos, populateTodos }
