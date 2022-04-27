1c8949c021784d530be1441da3a4bb2626dbc2bc
unknown <okorojiebube2@gmail.com>
2022-04-27 16:26:53 -0600

    Fix linter errors

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,41 @@
-const path = require('path')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-	mode: 'production',
-	entry: './src/index.js',
-	devServer: {
-		static: './dist',
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: './src/index.html',
-			favicon: './assets/images/favicon.ico',
-		}),
-		new CopyPlugin({
-			patterns: [{ from: './assets/images/capture.png', to: 'images' }],
-		}),
-	],
-	output: {
-		filename: 'bundle.js',
-		path: path.resolve(__dirname, 'dist'),
-		clean: true,
-	},
-	module: {
-		rules: [
-			{
-				test: /\.css$/i,
-				use: ['style-loader', 'css-loader'],
-			},
-			{
-				test: /\.(png|svg|jpg|jpeg|gif)$/i,
-				type: 'asset/inline',
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/i,
-				type: 'asset/inline',
-			},
-		],
-	},
-}
+  mode: 'production',
+  entry: './src/index.js',
+  devServer: {
+    static: './dist',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      favicon: './assets/images/favicon.ico',
+    }),
+    new CopyPlugin({
+      patterns: [{ from: './assets/images/capture.png', to: 'images' }],
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/inline',
+      },
+    ],
+  },
+};
